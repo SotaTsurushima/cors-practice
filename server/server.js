4beadc0e38818b23bf31c4148c9916a9dd076d7a
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-10-18 23:55:55 +0900

    取得するレスポンスの準備

--- server/server.js
+++ server/server.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
   res.send("Hello")
 })
 
+// response
+app.get("/data", (req, res) => {
+  res.json({ name: "私の名前", age: 23 })
+})
+
 app.listen(PORT, () => console.log("server is running"))
