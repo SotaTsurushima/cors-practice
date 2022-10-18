b77a66e2a8f594f7116ddf25d0dea4f4adabed76
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-10-19 00:02:14 +0900

    corsエラーの発生に成功

--- client/script.js
+++ client/script.js
@@ -1,1 +1,7 @@
-console.log("我輩");
+const fetchDataFromServer = () => {
+  fetch("http://localhost:5000/data")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+}
+
+fetchDataFromServer()
